baa431a5643064c36573f7a1cbe47fbf3dc96d96
saket_14 <mishrasaket042@gmail.com>
2025-02-05 04:09:51 +0000

    function .cpp file add

---
+++ 01 Introduction to c++/function.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+void Printcounting(){
+    for(int i=0;i<=5;i++){
+        cout<<i<<"  ";
+
+    }
+    cout<<endl;
+}
+int sum(int a,int b){
+   int result=a+b;
+    return result;
+}
+void myname();
+int multiplication(int x,int y,int z){
+    int totalvalue=x*y*z;
+    return totalvalue;
+}
+
+
+int main(){
+    Printcounting();
+    myname();
+   int totalsum= sum(5,6);
+   cout<<totalsum<<endl;
+   int totalvalue=multiplication(3,4,5);
+   cout<<totalvalue;
+
+ return 0;
+}
+void myname(){
+    cout<<"yash mishra"<<endl;
+}
