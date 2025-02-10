264509c52e25585d7c27ee14fc1a0d67535efaf8
saketmishra14 <mishrasaket042@gmail.com>
2025-02-11 00:21:59 +0530

    sort zero and one by using countig

---
+++ 04 Arrays, Time complexity and Space complexity/Array class-2/Sortzeroandone.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+void printZeroesAndOnes(int arr[],int size){
+    int countzero=0;
+    int countone=0;
+    //counting 
+    for(int i=0;i<size;i++){
+        if(arr[i]==0)
+        countzero++;
+        else{
+            countone++;
+        }
+    }
+    //sort one and zero
+    int i=0;
+    for (; i < countzero; i++)
+    {
+        arr[i]=0;
+    }
+    for (int i = countzero; i < size; i++)
+    {
+    arr[i]=1;
+    }
+    
+    
+}
+int main(){
+    int arr[]={0,1,1,1,0,0,1,1};
+    int size=8;
+    printZeroesAndOnes(arr,size);
+    for(int i=0; i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+   
+
+}
