b3b857c1d799e85cc54d82899c2fb45847681bdd
yash mishra <mishrasaket042@gmail.com>
2025-02-25 23:40:17 +0530

    updating sorting questions

---
+++ 06 Searching and Sorting/Doubt class/peakindex.cpp
@@ -0,0 +1,30 @@
+//leetcode 852: peak index in a mountain array
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+    public:
+        int peakIndexInMountainArray(vector<int>& arr) {
+            int ans=-1;
+            int start=0;
+            int end= arr.size() -1;
+            while(start<=end){
+                int mid = (start + end)>>1;
+                if(arr[mid]< arr[mid+1]){
+                    //peak at right side
+                    start=mid+1;
+                }
+                else if(arr[mid] < arr[mid-1]){
+                    //peak at left side
+                    end = mid-1;
+                }
+                else{
+                    ans=mid;
+                    break;
+                }
+            }
+            return ans;
+        }
+    };
