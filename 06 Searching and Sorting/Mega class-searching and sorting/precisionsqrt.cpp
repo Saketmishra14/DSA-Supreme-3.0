488a6298e13fe13a25863c7d2535b2119ebd9bca
yash mishra <mishrasaket042@gmail.com>
2025-03-02 23:08:29 +0530

    updating question

--- 06 Searching and Sorting/Mega class-searching and sorting/precisionsqrt.cpp
+++ 06 Searching and Sorting/Mega class-searching and sorting/precisionsqrt.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int MySquareRoot(int n){
     int s=0;
     int e=n;
-    int mid=s+(e-s)>>1;
+    int mid=s+((e-s)>>1);
     int ans=0;
-    while(s<e){
+    while(s<=e){
         if(mid*mid<=n){
             ans=mid;
             s=mid+1;
@@ -13,13 +13,32 @@ int MySquareRoot(int n){
         else{
             e=mid-1;
         }
-        mid=s+(e-s)>>2;
+        mid=s+((e-s)>>1);
     }
     return ans;
 }
+double Precisionsqrt(int n){
+    double sqrt=MySquareRoot(n);
+    int precision =10;
+    double step=0.1;
+    while(precision --){
+        double j=sqrt;
+        while(j*j<=n){
+            sqrt=j;
+            j=j+step;
+
+        }
+        step/=10;
+    }
+    return sqrt;
+}
 int main(){
-    int SquareRoot=100;
+    int SquareRoot=8;
     int result=MySquareRoot(SquareRoot);
     cout<<"square root is-:"<<result;
+    double ans = Precisionsqrt(SquareRoot);
+    //cout<<ans<<endl; //cout prints only upto 5 precision
+    //to print precision more than 5:
+    printf("Precision sqrt:  %.8f\n", ans);
     return 0;
 }
