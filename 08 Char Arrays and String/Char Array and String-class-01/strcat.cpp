8e91d5e00fd5d241b63e8b1d6d54206f02d13d88
saket_14 <mishrasaket042@gmail.com>
2025-03-09 18:02:23 +0000

    update

---
+++ 08 Char Arrays and String/Char Array and String-class-01/strcat.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+    char arr1[100];
+    cout<<"enter the input: "<<endl;
+    cin>>arr1;
+    char arr2[100];
+    cout<<"enter the input: "<<endl;
+    cin>>arr2;
+    cout<<strcat(arr1,arr2)<<endl;
+    cout<<strlen(arr1)<<endl;
+    return 0;
+}
