79f358e8e5bb8d2dd2981a3e69687fa51cccccdc
saketmishra14 <mishrasaket042@gmail.com>
2025-06-23 16:24:13 +0530

    question update

---
+++ 11 Backtracking & Dnc/Backtracking & DNC class-03/permutation.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+void Printpermutation(string str,int i){
+    //base case
+    if(i>=str.length()){
+        cout<<str<<endl;
+        return ;
+    }
+
+    for(int j=i;j<str.length();j++){
+        //ek case mai solve krunga baki ka case recursion sambhalega
+        swap(str[i],str[j]);
+
+        Printpermutation(str,i+1);
+    }
+
+}
+int main(){
+    string str="abc";
+    int i=0;
+    Printpermutation( str,i);
+
+    return 0;
+}
