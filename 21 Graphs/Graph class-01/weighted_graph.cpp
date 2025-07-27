2391cfa5944ba4f0dd8a9fc0d4e301fcdcda2d42
saketmishra14 <mishrasaket042@gmail.com>
2025-07-28 03:57:04 +0530

    graph basics

--- 21 Graphs/Graph class-01/weighted_graph.cpp
+++ 21 Graphs/Graph class-01/weighted_graph.cpp
@@ -4,18 +4,20 @@
 using namespace std;
 class Graph{
     public:
-    unordered_map<int, list<int>> adjList;
+    // first int: u,
+    // second int: v,
+    // third int: wt
+    unordered_map<int, list<pair<int,int>>> adjList;
 
-    void addedge(int u,int v,bool direction){
+    void addedge(int u,int v,int weight,bool direction){
         //direction->0 undirected graph
         //direction->1 directed graph
         if(direction==0){
-            adjList[u].push_back(u);
-            adjList[v].push_back(v);
+            adjList[u].push_back({u,weight});
+            adjList[v].push_back({v,weight});
         }
         else{
-            //directed edge
-            adjList[u].push_back(v);
+            adjList[u].push_back({v,weight});
         }
     }
 
@@ -23,9 +25,10 @@ class Graph{
         for(int i=0;i<n;i++){
             cout<<i<<" :";
             cout<<"{";
-            list<int> temp=adjList[i];
+            list<pair<int,int>> temp=adjList[i];
             for(auto j:temp){
-                cout<<j<<",";
+             // j is a pair here
+                cout<<"("<<j.first<<","<<j.second<<")";
             }
             cout<<"}"<<endl;
         }
@@ -44,10 +47,10 @@ class Graph{
 };
 int main(){ 
     Graph g;
-    g.addedge(0,1,1);
-    g.addedge(0,2,1);
-    g.addedge(1,2,1);
-    g.addedge(2,3,1);
+    g.addedge(0,1,3,1);
+    g.addedge(0,2,4,1);
+    g.addedge(1,2,1,1);
+    g.addedge(2,3,6,1);
     g.PrintadjList(4);
 
     return 0;
